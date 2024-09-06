91207b4beff64088a1df7c33f6290a913bbf17f2
Claudio Gabriel <claudiogaribeiro@gmail.com>
2024-09-06 20:43:26 -0300

    Adicionando Imagens e Função Mostrar Todos

--- app.js
+++ app.js
@@ -1,22 +1,69 @@
 function pesquisar() {
 
-let section = document.getElementById("resultados-pesquisa")
+let section = document.getElementById("resultados-pesquisa");
 
-let resultados = ""
+let campoPesquisa = document.getElementById("campo-pesquisa").value
+campoPesquisa = campoPesquisa.toLowerCase()
+
+if (campoPesquisa == ""){
+    section.innerHTML = "<p>Nenhum Mc foi encontrado</p>"
+    return
+}
+
+let resultados = "";
+let titulo = "";
+let descricao = "";
 
 for (let dado of artistas ) {
-    resultados += `
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLocaleLowerCase()
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+        resultados += `
   <div class="item-resultado">
+             <div class="item-resultado-imagem">
+                        <img src="${dado.imagem}"  alt="${dado.alt}">
+                    </div>  
+            <div class="item-resultado-infos">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
                 </h2>
                 <p class="descricao-meta">${dado.descricao}</p>
-                <a href=${dado.link} target="_blank">Mais informações</a>
-                <a href=${dado.instagram} target="_blank">Instagram</a>
+                <p class="titulos-mcs">${dado.titulos}</p>
+                <a class="instagram"href=${dado.instagram} target="_blank">Instagram</a>
             </div>
-`   
+</div>
+`;
+    }
+}
+
+if (!resultados) {
+    resultados = "<p>Nenhum Mc foi encontrado</p>"
 }
 
 section.innerHTML = resultados
+}
 
+function mostrarTodos() {
+    let section = document.getElementById("resultados-pesquisa");
+    let resultados = "";
+
+    artistas.forEach(artista => {
+        resultados += `
+            <div class="item-resultado">
+                <div class="item-resultado-imagem">
+                    <img src="${artista.imagem}" alt="${artista.alt}">
+                </div> 
+                <div class="item-resultado-infos">
+                    <h2>
+                        <a href="#" target="_blank">${artista.titulo}</a>
+                    </h2>
+                    <p class="descricao-meta">${artista.descricao}</p>
+                    <p class="titulos-mcs">${artista.titulos}</p>
+                    <a class="instagram" href="${artista.instagram}" target="_blank">Instagram</a>
+                </div>
+            </div>
+        `;
+    });
+
+    section.innerHTML = resultados;
 }
---
+++ artistas.js
@@ -0,0 +1,45 @@
+let artistas = [
+    {
+        titulo : "Cesar Mc", 
+        imagem: "cesar-mc-icon.jpg",
+        alt: "cesar-icon",
+        descricao : "César MC é um dos gigantes do freestyle no Brasil quando o assunto é agressividade nas rimas, conhecido por suas rimas complexas e improvisações ágeis. Sua influência na cena do rap brasileiro é inegável.",
+        titulos : "Titulos: Campeão Nacional 2018 e 2x Campeão BDA de Trio Aniversario",
+        instagram : "https://www.instagram.com/cesarmc027/"
+    }, 
+    {
+        titulo: "Emicida",
+        imagem: "emicida-icon.jpg",
+        alt: "Emicida",
+        descricao: "Emicida é um dos nomes mais influentes do rap brasileiro, conhecido por suas letras incisivas e sua abordagem única ao freestyle. Além de ser um MC respeitado, ele é um artista multifacetado que também atua como compositor e ativista social.",
+        titulos: "Titulos: Campeão Nacional 2011 e Melhor Artista Nacional 2016",
+        instagram : "https://www.instagram.com/emicida/"
+    }
+    , 
+    {
+        titulo: "Jotape",
+        imagem: "jota-mc-icon.jpg",
+        alt: "Jotape Icon",
+        descricao: "Jotape, ou mano underground é um renomado MC e freestyle, conhecido por sua habilidade técnica e pela presença marcante nas batalhas de rap. Com uma carreira sólida, ele é respeitado tanto por sua contribuição ao freestyle quanto pelo impacto cultural na cena do rap.",
+        titulos: "Titulos: 2x Campeão BDA de Trio Aniversario (2020,2023), Campeão da Freestyle Master Series 2024 e Campeão da Norte Ragnarok 2023 ",
+        instagram : "https://www.instagram.com/jotape4real?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+    }
+    ,
+    {
+        titulo: "MC Neo",
+        imagem: "neo mc.jpg",
+        alt: "Neo icon",
+        descricao: "MC Neo é um dos principais nomes da atual geração de MCs, conhecido por suas rimas rápidas e criativas, além de sua presença de palco marcante. Faz parte do primeiro Team de RAP da org MIBR junto de Apollo e Maria ZN. Ele vem conquistando cada vez mais fãs e quando o assunto é titulos por muitos e dito como o Maior MC da Historia do Freestyle Nacional.",
+        titulos: "Titulos: 3x Campeão Estadual no RJ, 3x Campeão da BDA de Trio Aniversario e2x Campeão Mar de Monstros", 
+        instagram: "https://www.instagram.com/neobxd/" 
+    }
+    ,
+    {
+        titulo: "Apollo",
+        imagem: "apollo-icon.jpg",
+        alt: "apollo-icon",
+        descricao: "Apollo começou sua jornada nas ruas, onde aprimorou suas habilidades em batalhas de rima. Sua capacidade de criar rimas impactantes e envolventes logo chamou a atenção, levando-o a competir em eventos maiores e mais profissionais. Um marco significativo em sua carreira foi a vitória na Batalha da Aldeia, a maior competição de freestyle do Brasil.",
+        titulos: "Titulos: 1x Campeão Estadual e 2x Campeão BDA de Trio Aniversario", 
+        instagram: "https://www.instagram.com/apollo.emici/" 
+    }
+];
